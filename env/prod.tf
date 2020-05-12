191b16d7d7280ee76c0d3bf72d5bcc883a27c8cf
Maciej Michalski <5445923+spy86@users.noreply.github.com>
2020-05-12 20:25:26 +0200

    Update prod.tf

--- env/prod.tf
+++ env/prod.tf
@@ -10,26 +10,8 @@ resource_owner = ""
 rg_location = ""
 rg_name = ""
 ###########################
-# ACR vars
-###########################
-application_insights_name = ""
-application_insights_type = ""
-###########################
-# Storage vars
-###########################
-storage_account_name = ""
-storage_account_tier = ""
-storage_account_replication_type = ""
-storage_container_name = ""
-###########################
 # IoT Hub vars
 ###########################
 iothub_name = ""
 iothub_sku_name = ""
 iothub_sku_capacity = ""
-###########################
-# Event Hub vars
-###########################
-eventhub_namespace_name = ""
-eventhub_namespace_sku = ""
-eventhub_authorization_rule_name = ""
